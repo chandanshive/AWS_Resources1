90ccba90477bf78cd77c68b49b49ef976387ebea
AKSHAY ASHOK CHANDANSHIVE <49795892+chandanshive@users.noreply.github.com>
2021-09-14 12:34:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "webserver" {
   ami           = "ami-01e36b7901e884a10"
-  instance_type = "t2.micro"
+  instance_type = "t2.large"
 
   tags = {
     Name = "webserver"
@@ -15,6 +15,6 @@ resource "aws_s3_bucket" "accuricsbucketdemo" {
   bucket = "my-tf-test-bucket"
 
   tags = {
-    Name = "bucketdemo"
+    Name = "bucket22demo"
   }
 }
